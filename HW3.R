edef87dd453575d67ccb41901f6f7a2a95ce8c97
mpmp2013 <mingpei2013@163.com>
2017-10-18 15:33:51 +0800

    hw3

--- HW3.R
+++ HW3.R
@@ -4,3 +4,10 @@ library(digest)
 #HW3.1
 digest("I learn a lot when I am listening to the professor properly","sha256")
 digest("I do not learn a lot when I am not listening to the professor properly","sha256")
+
+install.packages("rjson", repos="http://cran.us.r-project.org")
+library(rjson)
+json_file = "http://crix.hu-berlin.de/data/crix.json"
+json_data = fromJSON(file=json_file)
+crix_data_frame=as.data.frame(json_data)
+crix_data_frame[["date"]]
